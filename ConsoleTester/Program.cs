6d71d176624a33a0f10848918eed672e8f2ea14b
Daniel Bendel <daniel.bendel@gmail.com>
2013-09-07 17:44:00 +0200

    deferred test

--- ConsoleTester/Program.cs
+++ ConsoleTester/Program.cs
@@ -9,11 +9,17 @@ namespace ConsoleTester {
 		static void Main(string[] args) {
 			var promise = new Deferred<string, string>(MyMethod, "my promise").Promise().Done(SuccessCallback).Fail(FailCallback).Always(AlwaysCallback);
 			var promise2 = new Deferred<string, string>(MyMethod2, "my promise").Promise().Done(SuccessCallback).Always(AlwaysCallback).Fail(FailCallback);
+
+			Console.WriteLine("waiting a second ...");
+			Thread.Sleep(1000);
+			promise2.Always((result) => { Console.WriteLine("deferred always"); });
 			Console.ReadLine();
 		}
 
 		private static void MyMethod2(Deferred<string, string> deferred, string obj) {
 			Console.WriteLine(obj);
+			Thread.Sleep(500);
+			Console.WriteLine("thread awake");
 			deferred.Resolve(obj);
 		}
 
